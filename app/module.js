51470fb0405c8d08d8f7d5d9c122a7c30b0ed5a7
Tsimur_Sibahatau <tsimur_sibahatau@epam.com>
2017-02-07 18:08:18 +0300

    еу

--- app/module.js
+++ app/module.js
@@ -4,14 +4,26 @@ import articleListController from './controllers/ArticleListController'
 export default module('blog-app',[])
     .component('articles', {
         templateUrl: "../templates/articleList.html",
+        bindings: {
+            selectItem: "&",
+            articleList: "<"
+            
+        }
+
+    })
+    .component('rootComponent', {
+        templateUrl: "../templates/compose.html",
         controllerAs: 'articleListCtrl',
         controller: articleListController
     })
     .directive('articleForm', function(){
         return {
             templateUrl: "../templates/articleForm.html",
-            controllerAs: 'articleListCtrl',
-            controller: 'articleListController'
+            scope: {
+                selectedItem: "<"
+            },
+            controller: articleListController,
+            controllerAs: 'articleListCtrl'
         }
     }).
     controller('articleListController',articleListController)
